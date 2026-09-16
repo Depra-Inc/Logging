4028f4cb436ebdbb3180bab40ec7e41e8687ee22
n.melnikov <n.melnikov@depra.org>
2026-09-17 02:05:09 +0400

    Fix StringFormatMethodAttribute

---
+++ Logging/Attributes/StringFormatMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JetBrains.Annotations
+{
+	[AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method |
+	                AttributeTargets.Property | AttributeTargets.Delegate)]
+	internal sealed class StringFormatMethodAttribute : Attribute
+	{
+		public StringFormatMethodAttribute(string formatParameterName)
+			=> FormatParameterName = formatParameterName;
+
+		public string FormatParameterName { get; }
+	}
+}
